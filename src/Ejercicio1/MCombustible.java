786aeb09cb08248ad4f52ce72c0d56d228a5faa4
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-10-01 23:15:50 +0000

    S02

--- src/Ejercicio1/MCombustible.java
+++
@@ -1,6 +0,0 @@
-public abstract class MCombustible extends Motor {
-    /* Clase absrtracta Motores de Combustion */
-    public MCombustible(String marca, String numSerie, double precioM) {
-        super(marca, numSerie, precioM);
-    }
-}
